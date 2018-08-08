1c7a13cf2cf042489b8eac4ba44ca896c8255be8
Anastasiya Shtrakh <a4evo@yandex.ru>
2018-08-09 02:01:35 +0300

    fix: ARIA improvements

--- js/main.js
+++ js/main.js
@@ -205,6 +205,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
+	more.setAttribute('aria-label', 'View Details about ' + restaurant.name + ' restaurant');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
